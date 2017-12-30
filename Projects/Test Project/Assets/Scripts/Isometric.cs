8e4449ebc19332bf751d5b5b16a80b8c0e8867e9
Sean O'Connor <senoconn@gmail.com>
2017-12-30 14:49:57 -0700

    Added a script to deal with isometric objects

---
+++ Projects/Test Project/Assets/Scripts/Isometric.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Isometric : MonoBehaviour {
+
+    public float _floorHeight;
+    private readonly float angle = Mathf.Tan(Mathf.PI / 4);
+
+    private float _spriteLowerBound;
+
+	void Start () {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteLowerBound = spriteRenderer.bounds.size.y * 0.5f;
+	}
+	
+
+	void LateUpdate () {
+        float z = (transform.position.y - _spriteLowerBound + _floorHeight) * angle;
+        transform.position = new Vector3(transform.position.x, transform.position.y, z);
+	}
+}
