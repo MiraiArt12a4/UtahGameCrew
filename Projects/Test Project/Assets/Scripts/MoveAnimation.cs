333af4a301d78dd00e8f41543c8ebbe0a620395c
Sean O'Connor <senoconn@gmail.com>
2018-01-03 19:14:46 -0700

    Animation move script

--- Projects/Test Project/Assets/Scripts/MoveAnimation.cs
+++ Projects/Test Project/Assets/Scripts/MoveAnimation.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class MoveAnimation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    public Animator _animator;
+    public Animations _currentAnimation;
+	public enum Animations
+    {
+        Idle,
+        LeftWalk,
+        RightWalk,
+        UpWalk,
+        DownWalk
+    }
+
 	void Update () {
-		
-	}
+		if(Input.GetKey(KeyCode.A))
+        {
+            transition(Animations.LeftWalk, "Pinky_Walk_Left");
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transition(Animations.UpWalk, "Pinky_Walk_Up");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transition(Animations.DownWalk, "Pinky_Walk_Down");
+        }
+    }
+
+    public void transition(Animations anim, string name)
+    {
+        if(_currentAnimation != anim)
+        {
+            _animator.Play(name);
+            _currentAnimation = anim;
+        }
+    }
 }
