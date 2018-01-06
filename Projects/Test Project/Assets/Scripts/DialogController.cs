0a631721b9a3af4fc9782edc9df5bff8a70fb413
Sean O'Connor <senoconn@gmail.com>
2018-01-06 00:13:22 -0700

    Dialog box will appear when you get close to Connie, and disappear as you go away from her.

---
+++ Projects/Test Project/Assets/Scripts/DialogController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour {
+
+    [SerializeField]
+    Canvas _dialogBubble;
+    [SerializeField]
+    Text _text;
+    [SerializeField]
+    TextAsset _textFile;
+	// Use this for initialization
+	void Start () {
+        _dialogBubble.enabled = false;
+        _text.supportRichText = false;
+        _text.text = _textFile.text;
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //_dialogBubble.enabled = true;
+        _text.supportRichText = true;
+        if (collision.name == "Pinky")
+        {
+            _dialogBubble.enabled = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        //_dialogBubble.enabled = false;
+        if (collision.name == "Pinky")
+        {
+            _dialogBubble.enabled = false;
+        }
+    }
+}
