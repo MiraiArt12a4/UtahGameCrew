e6708bada4f23dee07b164a9d0b45a22b22df981
Mirai Mckain <miraiweirdmckain@gmail.com>
2018-01-22 18:20:32 -0700

    Tiled in Projects Folder

---
+++ Projects/Tiled/Test Dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Dungeon Tiles" tilewidth="100" tileheight="100" tilecount="100" columns="10">
+ <image source="../Desktop/CastleTiles01.png" trans="ff00ff" width="1000" height="1000"/>
+</tileset>
