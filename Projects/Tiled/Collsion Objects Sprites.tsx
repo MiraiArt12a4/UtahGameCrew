8989c4589bd0ae5bae3cab6f5466d232d8fbf4cf
Sean O'Connor <senoconn@gmail.com>
2018-01-24 19:53:46 -0700

    added tilesets

---
+++ Projects/Tiled/Collsion Objects Sprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Collsion Objects Sprites" tilewidth="200" tileheight="200" tilecount="25" columns="5">
+ <image source="Collsion Objects Sprites.png" width="1000" height="1000"/>
+</tileset>
---
+++ Projects/Tiled/Isometric Base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Isometric Base" tilewidth="200" tileheight="200" tilecount="25" columns="5">
+ <image source="Isometric Base.png" width="1000" height="1000"/>
+</tileset>
