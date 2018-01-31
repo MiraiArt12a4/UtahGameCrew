f2dbb213cb1ea01b2146a7459d9e4d1ab14ae3e1
Sean O'Connor <senoconn@gmail.com>
2018-01-30 23:10:30 -0700

    Place tiles onto puzzle image

---
+++ Projects/Test Project/Assets/Scripts/Puzzle_Scripts/Puzzle3Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puzzle3Controller : PuzzleControllerBase
+{
+    public List<Sprite> _list;
+
+    [SerializeField]
+    private Image _dialImage1;
+    [SerializeField]
+    private Image _dialImage2;
+    [SerializeField]
+    private Image _dialImage3;
+
+    void Start () {
+
+       foreach(Sprite s in _list)
+        {
+
+        }
+    }
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+    }
+
+    private void TransitionDials()
+    {
+
+    }
+}
+
