6d5f688f114e05e1fb1fc8469d3970487a5e8de8
Mirai Mckain <miraiweirdmckain@gmail.com>
2018-02-09 18:06:17 -0700

    School map in tiled folder
    Sync

---
+++ Projects/Tiled/School Objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="School Objects" tilewidth="200" tileheight="200" tilecount="25" columns="5">
+ <image source="School Objects.png" width="1000" height="1000"/>
+</tileset>
