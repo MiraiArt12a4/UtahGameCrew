bfa4e30147a9720362279e6bcbf6015aadfbff5c
Sean O'Connor <senoconn@gmail.com>
2018-02-23 00:28:58 -0700

    Dial puzzle scripts done

--- Projects/Test Project/Assets/Scripts/Puzzle_Scripts/Puzzle3Controller.cs
+++ Projects/Test Project/Assets/Scripts/Puzzle_Scripts/Puzzle3Controller.cs
@@ -6,25 +6,20 @@ using UnityEngine.UI;
 public class Puzzle3Controller : PuzzleControllerBase
 {
     public List<Sprite> _list;
+    public List<Image> _dialList;
 
-    [SerializeField]
-    private Image _dialImage1;
-    [SerializeField]
-    private Image _dialImage2;
-    [SerializeField]
-    private Image _dialImage3;
-
+    private List<int> _dialIndexes;
     private int _currentDial;
-    private int _dialIndex1;
-    private int _dialIndex2;
-    private int _dialIndex3;
 
     void Start()
     {
+        _dialIndexes = new List<int>();
         _currentDial = 0;
-        _dialIndex1 = 0;
-        _dialIndex2 = 0;
-        _dialIndex3 = 0;
+
+        foreach(Image i in _dialList)
+        {
+            _dialIndexes.Add(0);
+        }
     }
 
     void Update()
@@ -34,95 +29,44 @@ public class Puzzle3Controller : PuzzleControllerBase
             _currentDial--;
             if (_currentDial < 0)
             {
-                _currentDial = 2;
+                _currentDial = _dialList.Count - 1;
             }
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
-            GetNextDialIndex(_currentDial, true);
+            _dialIndexes[_currentDial] = bounds(_dialIndexes[_currentDial] + 1);
+
+            _dialList[_currentDial].sprite = _list[_dialIndexes[_currentDial]];
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            GetNextDialIndex(_currentDial, false);
+            _dialIndexes[_currentDial] = bounds(_dialIndexes[_currentDial] - 1);
+            _dialList[_currentDial].sprite = _list[_dialIndexes[_currentDial]];
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             _currentDial++;
-            if (_currentDial > 2)
+            if (_currentDial > _dialList.Count - 1)
             {
                 _currentDial = 0;
             }
         }
     }
 
-    private void GetNextDialIndex(int index, bool isNext)
+    private int bounds(int i)
     {
-        switch (index)
+        if(i < 0)
         {
-            case 0:
-                if (isNext)
-                {
-                    _dialIndex1++;
-                    if (_dialIndex1 > _list.Count - 1)
-                    {
-                        _dialIndex1 = 0;
-                    }
-                }
-                else
-                {
-                    _dialIndex1--;
-                    if (_dialIndex1 < 0)
-                    {
-                        _dialIndex1 = _list.Count - 1;
-                    }
-                }
-                Debug.Log(_dialIndex1);
-                _dialImage1.sprite = _list[_dialIndex1];
-                break;
-
-            case 1:
-                if (isNext)
-                {
-                    _dialIndex2++;
-                    if (_dialIndex2 > _list.Count - 1)
-                    {
-                        _dialIndex2 = 0;
-                    }
-                }
-                else
-                {
-                    _dialIndex2--;
-                    if (_dialIndex2 < 0)
-                    {
-                        _dialIndex2 = _list.Count - 1;
-                    }
-                }
-                Debug.Log(_dialIndex2);
-                _dialImage2.sprite = _list[_dialIndex2];
-                break;
-
-            case 2:
-                if (isNext)
-                {
-                    _dialIndex3++;
-                    if (_dialIndex3 > _list.Count - 1)
-                    {
-                        _dialIndex3 = 0;
-                    }
-                }
-                else
-                {
-                    _dialIndex3--;
-                    if (_dialIndex3 < 0)
-                    {
-                        _dialIndex3 = _list.Count - 1;
-                    }
-                }
-                Debug.Log(_dialIndex3);
-                _dialImage3.sprite = _list[_dialIndex3];
-                break;
+            return _list.Count - 1;
         }
-
+        else if(i > _list.Count - 1)
+        {
+            return 0;
+        }
+        else
+        {
+            return i;
+        }
     }
 }
 
