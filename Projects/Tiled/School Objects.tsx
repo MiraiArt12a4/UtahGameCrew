c18c0b0f07ef06506064f56f4609facce9dabd9e
Mirai Mckain <miraiweirdmckain@gmail.com>
2018-02-23 18:39:40 -0700

    TIled Object Collisions

--- Projects/Tiled/School Objects.tsx
+++ Projects/Tiled/School Objects.tsx
@@ -1,4 +1,102 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="School Objects" tilewidth="200" tileheight="200" tilecount="25" columns="5">
  <image source="School Objects.png" width="1000" height="1000"/>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="0.545455" y="148.909">
+    <polygon points="0,0 49.4545,25.0909 150.182,-24.9091 99.4545,-49.8182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="0.363636" y="150.909">
+    <polygon points="0,0 25.0909,12.9091 125.273,-38 98.7273,-50.7273"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="74.7273" y="111.818">
+    <polygon points="0,0 25.6364,-12.1818 124.364,38 100.545,50.3636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="74.7273" y="112.182">
+    <polygon points="0,0 99.0909,50 125.455,36.7273 25.6364,-12.7273"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="0.363636" y="147.818">
+    <polygon points="0,0 24.5455,13.4545 125.091,-36.3636 99.4545,-50"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="76" y="112">
+    <polygon points="0,0 98.7273,49.4545 123.636,35.8182 24.3636,-13.2727"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="25.0909" y="137.091">
+    <polygon points="0,0 48.7273,24.7273 100.182,-0.909091 50,-23.8182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index">
+   <object id="1" x="77.8182" y="137.636">
+    <polygon points="0,0 50,24.5455 99.2727,0.363636 50,-24.1818"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index">
+   <object id="1" x="0.727273" y="148.727">
+    <polygon points="0,0 49.4545,25.6364 149.091,-24.7273 99.2727,-50"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index">
+   <object id="1" x="50.3636" y="124.727">
+    <polygon points="0,0 99.8182,49.0909 150,24.3636 49.2727,-25.6364"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index">
+   <object id="8" x="68.1818" y="104.364" width="64.3636" height="32">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index">
+   <object id="2" x="74.9091" y="112.364">
+    <polygon points="0,0 25.2727,12.1818 50.1818,-0.363636 25.2727,-13.6364"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index">
+   <object id="1" x="76" y="150.727">
+    <polygon points="0,0 24.3636,11.8182 47.8182,-0.727273 24.5455,-12.9091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index">
+   <object id="1" x="69.2727" y="150">
+    <polygon points="0,0 25.0909,12.9091 49.4545,0 24.9091,-12.7273"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
