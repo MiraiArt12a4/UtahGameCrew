6b77b33ae26ab2c506c72d0c388cf0f620602741
Mirai Mckain <miraiweirdmckain@gmail.com>
2018-02-26 18:35:54 -0700

    Sync Tiled

--- Projects/Tiled/Isometric Base.tsx
+++ Projects/Tiled/Isometric Base.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Isometric Base" tilewidth="200" tileheight="200" tilecount="25" columns="5">
  <image source="Isometric Base.png" width="1000" height="1000"/>
+ <tile id="18">
+  <objectgroup draworder="index">
+   <object id="3" x="100.067" y="100">
+    <polygon points="0,0 -100.129,50.0625 -0.00416667,100.016 99.9177,50"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
